56e49a7aeeebd2bd858ff52a4c55a2513c239865
Izaak Meckler <ihmeckler@gmail.com>
2019-10-29 18:08:26 -0700

    niceify

--- solutions/ex_preimage/index.js
+++ solutions/ex_preimage/index.js
@@ -3,16 +3,16 @@ const Snarky = require('snarkyjs');
 const snarky = new Snarky("./ex_preimage.exe");
 
 const preImage = bn128.Field.ofInt(5);
-const statement = bn128.Hash.hash([ preImage ]);
+const statement = [ bn128.Hash.hash([ preImage ]) ];
 
 snarky.prove({
-  statement: [ statement ],
+  statement: statement,
   witness: preImage
 }).then((proof) => {
   console.log("Created proof:\n" + proof + "\n");
   return snarky.verify({
-    "statement": [ statement ],
-    "proof": proof
+    statement: statement,
+    proof: proof
   });
 }, console.log).then((verified) => {
   console.log("Was the proof verified? " + verified);
